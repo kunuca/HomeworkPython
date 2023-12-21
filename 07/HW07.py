d2db0166cd51e3b0f825c76bab4ac6f4b79e5127
Tatiana Kustova <kunuca95@gmail.com>
2023-12-22 00:36:00 +0400

    7.0

--- 07/HW07.py
+++ 07/HW07.py
@@ -109,3 +109,17 @@
 # значение некоторой характеристики, и возвращают True, если это так. Если значение характеристики для разных объектов отличается 
 # - то False. Для пустого набора объектов, функция должна возвращать True. Аргумент characteristic - это функция, которая принимает 
 # объект и вычисляет его характеристику.
+
+def same_by(characteristic, objects):
+    if not objects:
+        return True
+    result=list(map(characteristic,objects))
+    print(result)
+    return all(result)==any(result)
+
+values=[0, 2, 10, 6]
+
+if same_by(lambda x: x%2==0, values):
+    print("same")
+else:
+    print("different")
